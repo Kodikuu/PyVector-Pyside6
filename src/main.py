23c9ab1f05eb0bfdd61ecf508484df0b284fe601
Kodikuu <callum.james.watson@gmail.com>
2020-12-30 14:05:20 +0000

    Create a window

--- src/main.py
+++ src/main.py
@@ -2,9 +2,20 @@ __author__ = "Callum Watson"
 __version__ = "0.0.1"
 __license__ = "The Unlicense"
 
+from PySide6.QtWidgets import QApplication, QWidget
+
+class Visualiser(QWidget):
+    def __init__(self):
+        super().__init__()
+
 
 def main():
-    pass
+    app = QApplication()
+
+    vis = Visualiser()
+    vis.show()
+
+    app.exec_()
 
 
 if __name__ == "__main__":
