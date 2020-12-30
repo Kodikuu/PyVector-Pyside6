32e3f151504bf9408b01d303f40dd9b69d86007a
Kodikuu <callum.james.watson@gmail.com>
2020-12-30 14:08:30 +0000

    Resize window and remove decorations + background

--- src/main.py
+++ src/main.py
@@ -3,16 +3,23 @@ __version__ = "0.0.1"
 __license__ = "The Unlicense"
 
 from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import *
 
 class Visualiser(QWidget):
     def __init__(self, parent, taskbar=30):
         super().__init__()
+
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
 
         self.parent = parent
         self.scr_width = self.parent.primaryScreen().size().width()
         self.scr_height = self.parent.primaryScreen().size().height()
 
         self.origin_y = self.scr_height - taskbar
+
+        self.resize(self.scr_width, self.scr_height)
 
 
 def main():
